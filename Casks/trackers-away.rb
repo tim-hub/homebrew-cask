ecd43a4e91dd8aab216cf1a2985ac5300a9f1e8f
tim-hub <tim_w@outlook.com>
2020-02-18 22:43:35 +1300

    add trackers away 0.2.3

---
+++ Casks/trackers-away.rb
@@ -0,0 +1,10 @@
+cask 'trackers-away' do
+  version '0.2.3'
+  sha256 'c6e41883718725bdbd6a6c1ce59ccc08565a8c395ea1ebb1705217168ee11eb8'
+
+  url "https://github.com/tim-hub/TrackersAway/releases/download/v0.2.3/trackers-away-0.2.3.dmg"
+  name 'Trackers Away'
+  homepage 'https://github.com/tim-hub/TrackersAway'
+
+  app 'trackers-away.app'
+end
